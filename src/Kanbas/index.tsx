41eba1b7dcc7b8e3f5b085d4856037bc04fdecac
garnier.n@northeastern.edu <Noah Garnier>
2024-02-21 15:20:28 -0500

    stuff done

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,12 +1,24 @@
 import Nav from "../Nav";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Courses from "./Courses";
+import KanbasNavigation from "./Navigation";
 
 function Kanbas() {
     return (
+      <div className="d-flex">
         <div>
-        <Nav />
-         <h1>Kanbas</h1>
+          <KanbasNavigation />
         </div>
-       )
-      
- }
- export default Kanbas;
+        <div style={{ flexGrow: 1 }}>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/:courseId/*" element={<Courses />} />
+        </Routes>
+
+        </div>
+      </div>
+  );}
+  export default Kanbas;
