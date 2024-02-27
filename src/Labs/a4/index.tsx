3c8ca3bd5d0f4bdd095f6d49aa5973d34d5852d7
garnier.n@northeastern.edu <Noah Garnier>
2024-02-26 23:11:28 -0500

    a4 lab section complete

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,36 @@
-function Assignment4() {
-    return (
-        <div>
-            <h2>Assignment 4</h2>
-        </div>
-    )
-}
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
+function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div>
+      <h1>Assignment 4</h1>
+      <ReduxExamples />
+      <PassingFunctions theFunction={sayHello} />
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+    </div>
+  );
+}
 export default Assignment4;
