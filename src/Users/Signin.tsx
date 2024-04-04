22538794209663ea075270acf72d256d58949d33
garnier.n@northeastern.edu <Noah Garnier>
2024-04-04 19:53:18 -0400

    a6 complete

---
+++ src/Users/Signin.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { User } from "./client";
+import * as client from "./client";
+export default function Signin() {
+  const [credentials, setCredentials] = useState<User>({ _id: "",
+    username: "", password: "", firstName: "", lastName: "", role: "USER"
+  });
+  const navigate = useNavigate();
+  const signin = async () => {
+    await client.signin(credentials);
+    navigate("/Kanbas/Account/Profile");
+  };
+  return (
+    <div>
+      <h1>Signin</h1>
+      <label>Username</label>
+      <br />
+      <input
+        value={credentials.username}
+        onChange={(e) =>
+          setCredentials({ ...credentials, username: e.target.value })
+        }
+      />
+      <br />
+      <br />
+      <label>Password</label>
+      <br />
+      <input
+        value={credentials.password}
+        onChange={(e) =>
+          setCredentials({ ...credentials, password: e.target.value })
+        }
+      />
+      <br />
+      <button onClick={signin}>Sign in</button>
+    </div>
+  );
+}
